56cbd40c0e9b8baa2a963690ef001ce637599d14
이휘경 <ter0000@naver.com>
2023-03-30 14:57:35 +0900

    🔧Fixed(Nav)  : URL 주소 변경

--- concert/src/components/Nav.js
+++ concert/src/components/Nav.js
@@ -1,19 +1,16 @@
-import { useEffect, useRef, useState } from 'react';
 import styled from './Nav.module.scss'
 import NavLogo from './Nav/NavLogo';
 import NavItem from './Nav/NavItem';
 
 
-function Nav() {
-
-  
+function Nav() {  
   return (
       <div className= {styled.navContainer}>
         <div className = {styled.NavItemContainer} >    
           <NavLogo navLogoImage={"Images/AnyConv.com__logo.WEBP"} navLink = {"/"}/>
           <NavItem itemName = {"HOME"} navLink = {"/"}/>
           <NavItem itemName = {"CALENDAR"} navLink = {"Calendar"}/>
-          <NavItem itemName = {"COMMUNITY"} navLink = {"/board/1"} color = 'red'/>
+          <NavItem itemName = {"COMMUNITY"} navLink = {"/boards/1?page=0"} color = 'red'/>
         </div>
     </div>
   );
